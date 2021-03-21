439f9abb7373e49509c84deafe09f98b3555b7f1
yibeiou <yibei.ou@gmail.com>
2021-03-21 12:38:09 +1100

    add print information in spark-start-cluster.sh

--- spark-start-cluster.sh
+++ spark-start-cluster.sh
@@ -5,8 +5,20 @@
 
 # This shell script file is also called by start-cluster.sh
 
+echo "Starting Spark Cluster: Master X 1, Worker X 3"
 /usr/local/spark/sbin/start-all.sh
+echo ""
 
+echo "Starting Spark History Server"
 /usr/local/spark/sbin/start-history-server.sh
+echo ""
 
 /bin/xcall jps
+
+echo ""
+
+echo "Spark Server http://namenode:8080"
+
+echo ""
+
+echo "Spark History Server http://namenode:4000"
